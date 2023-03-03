248e7448b45cca185b2548e02f0f9b634a364e9c
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-03 16:40:56 -0600

    Se agrego archivo index para acceder a todas las paginas, llevar un control de las paginas incluidas

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Dashboard from './Dashboard/dashboard';
+import Login from './Login/login-page';
+import Productos from './Productos/productos-page';
+import Usuarios from './Usuarios/usuarios-page.jsx';
+
+export {
+    Dashboard,
+    Login,
+    Productos,
+    Usuarios,
+}
