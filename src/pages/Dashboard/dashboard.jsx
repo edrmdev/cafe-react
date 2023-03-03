7f66967e7de6c59f1f2d8b9885d21a5094764354
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-03 16:37:01 -0600

    Se removio el menu de navegacion del dashboard para ser incluido de forma global

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,28 +1,12 @@
-import Grid from "@mui/material/Grid";
-import css from './dashboard.css';
 import ProductsList from "../../components/product-list/products-list";
-import MenuNavBar from "../../components/menu/menu";
-import { styles } from "../../styled-components/custom";
-import { Typography } from "@mui/material";
-
-const pages = ['Usuarios', 'Productos', 'Categorías'];
-const settings = ['Usuarios', 'Productos', 'Categorías', 'Cerrar Sesión'];
-const userName = "Example";//Replace this with redux global state
+import styles from './dashboard.css';
 
 const Dashboard = () => {
     
     return (
         <>
-            { 
-                <MenuNavBar title="Cafe" pages={pages} settings={settings} />
-            } 
-            <main>
-                <div>
-                    <div>
-                        <Typography component="h4" variant="h4">Welcome {userName}!</Typography>
-                    </div>
-                    <ProductsList />
-                </div>
+            <main>   
+                <ProductsList />
             </main>
         </>
     )
