b4426e6e37bfcecb57be5f95a2a3f7ddbca1b6f4
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-03 16:35:02 -0600

    Se incluyo BrowserRouter en el root de la app, para realizar redirecciones en rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  // <RouterProvider router={router} />
+
   //</React.StrictMode>,
 )
