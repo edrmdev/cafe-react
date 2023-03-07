b10a7b16b1098ee30a165802a6067753d39d8554
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-07 16:52:52 -0600

    Pagina para operaciones relacionadas con los productos

---
+++ src/pages/Productos/productos-page.jsx
@@ -0,0 +1,10 @@
+import ProductsList from "../../components/product-list/products-list";
+
+const ProductsPage = () => (
+    <>
+        <h1>Products page</h1>
+        <ProductsList cantidad_productos={3}/>
+    </>
+)
+
+export default ProductsPage;
