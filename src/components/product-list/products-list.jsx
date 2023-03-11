07a32a5135e0108f062aa9e275acf56c0257bfd8
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-10 21:11:39 -0600

    Recursos innecesarios removidos

--- src/components/product-list/products-list.jsx
+++ src/components/product-list/products-list.jsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from 'react';
-import { getProducts } from '../../services/public.service';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { getProductos } from '../../services/products.service';
 import { Button, CardActions } from '@mui/material';
 
 const ProductsList = ( { products, handleProductUpdate } ) => {
