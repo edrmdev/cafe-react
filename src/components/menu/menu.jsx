3965986fde6eb3ee682e81d5832e4803ec8aab0f
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-13 12:49:19 -0600

    Se agrego funcionalidad para el cierre de sesion actualizando estado redux, se modifico de array a array de objeto el parametro que recibe de pages y settings para establecer nombre, link e icono para opciones y visualizacion en menu de navegacion

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import ToolBar from '@mui/material/ToolBar';
@@ -13,13 +13,25 @@ import ToolTip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import CoffeIcon from '@mui/icons-material/Coffee';
 import { NavLink, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut } from '../../redux/actions/user-actions';
+import LogOutIcon from '@mui/icons-material/Logout';
 
 function MenuNavBar( props ) {
 
-  const { pages = [], settings = [], title = '', urls = [] } = props;
+  const { pages = [], settings = [], title = '' } = props;
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const { auth } = useSelector(state=> state.rootReducer);
+  const [user, setUser] = useState(null);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log( auth)
+    if( auth ) 
+      setUser(auth.user); 
+  })
   //Event handlers
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -29,7 +41,8 @@ function MenuNavBar( props ) {
     setAnchorElUser(event.currentTarget);
   }
 
-  const handleCloseNavMenu = (event) => {    
+  const handleCloseNavMenu = (event) => {
+    console.log(event);
     setAnchorElNav(null);
   }
 
@@ -38,15 +51,13 @@ function MenuNavBar( props ) {
   }
 
   return (
-    <AppBar position='static' sx={{ bgcolor: "brown"}}>
+    <AppBar position='static' sx={{ backgroundColor: "#7B3F00"}}>
       <Container maxWidth="x1">
         <ToolBar disableGutters>
           <CoffeIcon sx={{ display: {xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant='h6'
             noWrap
-            component="a"
-            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
@@ -94,9 +105,9 @@ function MenuNavBar( props ) {
                     <Typography textAlign="center">
                       <Link
                         style={{ textDecoration: 'none', color: 'white' }}
-                        to={`/${page}`}
+                        to={`/${page.link}`}
                       >
-                        {page}
+                        {page.name}
                       </Link>
                       </Typography>
                   </MenuItem>
@@ -125,24 +136,28 @@ function MenuNavBar( props ) {
             { title }
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }}} >
-          {
-            
+          { 
             pages.map((page, idx) => (
-              <Link to={`/${page}`} style={{ textDecoration: "none" }}>
+              <Link to={`/${page.link}`} key={idx} style={{ textDecoration: "none" }}>
                 <Button
-                  key={idx}
                   onClick={handleCloseNavMenu}
                   sx={{ my: 2, color: '#FFFF', display: 'block' }}> 
-                    { page }
+                    { page.name }
                 </Button>
               </Link>
             ))
           }
           </Box>
+          {
+            ( user ) ?
+            <Typography >
+              { `${user.nombre} | ${ user.rol }` }
+            </Typography> : null 
+          }
           <Box sx={{flexGrow: 0 }}>
             <ToolTip title="Open Settings">
               <IconButton onClick={handleOpenUserMenu}>
-                <Avatar alt="Settings" src="/" />
+                <Avatar alt="Settings" src={ (user ? user.img : "#" ) } />
               </IconButton>
             </ToolTip>
             <Menu
@@ -167,14 +182,22 @@ function MenuNavBar( props ) {
                     <Typography textAlign="center">
                     <Link
                         style={{ textDecoration: 'none' }}
-                        to={`/${setting}`}
+                        to={`/${setting.link}`}
                       >
-                        {setting}
+                        { setting.icon } &nbsp;
+                        {setting.name}
                       </Link>
                     </Typography>
                   </MenuItem>
                 ))
               }
+              
+              <MenuItem onClick={ () => dispatch( logOut() ) }>
+                  <Typography textAlign="center">
+                    <LogOutIcon />
+                    Cerrar Sesión&nbsp;
+                  </Typography>
+                </MenuItem>
             </Menu>
           </Box>
         </ToolBar>
