be86f4035d63b4aeefc6fcca3f6576c76f208c5a
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-13 13:06:20 -0600

    Se modifico la configuracion que se le envia al menu de navegacion de array a array de objetos, para poder incluir iconos en base al elemento del menu y se agrego el titulo

--- src/routes/withnav.jsx
+++ src/routes/withnav.jsx
@@ -2,14 +2,46 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import MenuNavBar from '../components/menu/menu';
+import UserIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CategoryIcon from '@mui/icons-material/Category';
+import HomeIcon from "@mui/icons-material/Home";
 
-const pages = ['Usuarios', 'Productos', 'Categorías'];
-const settings = ['Usuarios', 'Productos', 'Categorías', 'Cerrar Sesión'];
+const pages = [
+{
+  name: 'Inicio',
+  link: '',
+  icon: <HomeIcon />,
+},
+{
+  name: 'Usuarios',
+  link: 'Usuarios',
+  icon: <UserIcon />
+}, 
+{
+  name: 'Productos',
+  link: 'Productos',
+  icon: <ShoppingCartIcon />
+}, 
+{
+  name: 'Categorías', 
+  link: 'Categorias',
+  icon: <CategoryIcon />
+}];
+
+const settings = 
+  [
+    ...pages,
+    {
+      name: 'Cerrar Sesión',
+      link: 'LogOut'
+    }
+  ];
 
 export default () => {
   return (
     <>
-      <MenuNavBar pages = {pages} settings = {settings} />
+      <MenuNavBar pages = {pages} settings = {pages} title={"Cafe"} />
       <Outlet />
     </>
   );
