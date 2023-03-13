c41bf0ea80f065e1bad5d649fef91984b68c6049
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-13 12:52:17 -0600

    Manejador de estado para inicio de sesion (info de usuario y token)

--- src/pages/Login/login-page.jsx
+++ src/pages/Login/login-page.jsx
@@ -4,22 +4,21 @@ import {
     Card, 
     CardContent, 
     CardMedia, 
-    Container, 
-    FormLabel, 
-    Grid,
     TextField,
     Typography
  } from "@mui/material"; 
 import { useEffect, useState } from "react";
 import { authUser, setAuthorizationToken } from "../../services/auth.service";
 import { useNavigate } from 'react-router-dom';
+import store from "../../redux/store";
+import { logIn } from "../../redux/actions/user-actions";
 
 const LoginPage = () => {
     const [Email, setEmail] = useState("");
     const [Password, setPassword] = useState("");
     const [ErrorDescription, setErrorDescription] = useState(""); 
     let navigate = useNavigate();
-
+    
     const handleSendData = async () => {
         try{            
             const { usuario, token } = await authUser(Email, Password);
@@ -27,15 +26,16 @@ const LoginPage = () => {
             if( !token ){
 
                 setErrorDescription( 'Acceso no autorizado' );
-                throw new Error('Acceso no autorizado');
+                store.dispatch( loginFailure() );
+                throw new Error( 'Acceso no autorizado' );
             }
 
+            store.dispatch( logIn( usuario, token ));
             setAuthorizationToken( token );
-            
             navigate('/productos');
         }
         catch(err){
-
+            store.dispatch( loginFailure() );
             console.log(err);
             setErrorDescription( 'Acceso no autorizado' );
         }
