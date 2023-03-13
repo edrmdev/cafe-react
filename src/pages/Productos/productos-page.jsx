3af8847161e9b7b3da8bfdd8c6f0833de7e85a85
Edgar Ramirez <edgramir17@hotmail.com>
2023-03-13 13:09:41 -0600

    Se agrego una notificacion al suceder un error al modificar un producto y se agregaron eventos para modificar el producto invocando los eventos al child component ProductsList

--- src/pages/Productos/productos-page.jsx
+++ src/pages/Productos/productos-page.jsx
@@ -1,27 +1,26 @@
-import { Alert, Snackbar } from "@mui/material";
+import { 
+    Alert,
+    Container, 
+    Grid, 
+    Snackbar
+ } from "@mui/material";
 import { useEffect, useState } from "react";
 import ProductsList from "../../components/product-list/products-list";
 import { 
     getProductos,
     updateProducto,
  } from "../../services/products.service";
-
+import './productos-page.css';
 const ProductsPage = () => {
     
     const [Products, setProducts ] = useState([]);
     const [MensajeDeInformacion, setMensajeDeInformacion ] = useState('');
     const [openNotificacion, setOpenNotificacion] = useState(false);    
+    const [ProductoAModificar, setProductoAModificar] = useState('');
+    const [editando, setEditando] = useState(false);
     
-    const handleCloseNotification = (event, reason) => {
-        if(reason === 'clickaway'){
-            return;
-        }
-
-        setOpenNotificacion(false);
-    }
-
     useEffect(() => {
-
+        
         const obtenerProductos = async () => {
             const { productos } = await getProductos();
             setProducts(productos);
@@ -30,21 +29,33 @@ const ProductsPage = () => {
         obtenerProductos();
     }, []);
 
-    const handleModificarPrecio = (id) => {
+    const handleCloseNotification = (event, reason) => {
+        if(reason === 'clickaway'){
+            return;
+        }
 
+        setOpenNotificacion(false);
+    }
+
+
+    const handleEditar = (id) => {
+      const index = Products.findIndex( product => product._id === id );
+      const ProductoAEditar = Products[index];
+
+      setProductoAModificar( ProductoAEditar );
+      setEditando(true);
+    };
+  
+    const handleGuardar = (id) => {
+        setEditando(false);
         const index = Products.findIndex( (product) => product._id === id );
-        
+    
         if( index !== -1 ){
-
-            const updatedProduct = {
-                ...Products[index], 
-                precio: 33,
-            };
             
-            updateProducto( id, updatedProduct ).then((response) => {
+            updateProducto( id, ProductoAModificar ).then((response) => {
+                
                 const updatedProducts = [...Products];
-                updatedProducts[index] = updatedProduct;
-
+                updatedProducts[index] = ProductoAModificar;
                 setProducts( updatedProducts );
             })
             .catch((err)=>{
@@ -53,20 +64,49 @@ const ProductsPage = () => {
                 setOpenNotificacion( true );
                 console.error(data);
             });
-
         }
+    };
+  
+    const handleCancelar = () => {
+      setEditando(false);
+      setProductoAModificar('');
+    };
 
+    const handleProductChange = (updatedProduct) => {
+
+        setProductoAModificar(updatedProduct); 
     }
+
     
     return(
     <>
-        <h1>Products page</h1>
-        <ProductsList 
-            products = { Products }
-            handleProductUpdate={ handleModificarPrecio } 
-            />
-        <Snackbar open={openNotificacion} autoHideDuration={ 6000 } onClose={ handleCloseNotification }>
-            <Alert onClose={handleCloseNotification} severity="error" sx={{ width: '100%' }}>
+        <h1 className="titulo">Products</h1>
+        <Container>
+
+            <Grid container spacing={2}>
+                <Grid item md={8} sm={12} lg={12}>
+                <ProductsList 
+                    products = { Products }
+                    handleEditar={ handleEditar }
+                    handleGuardar={ handleGuardar}
+                    handleCancelar={ handleCancelar }
+                    nuevoProducto={ProductoAModificar}
+                    onProductChange={handleProductChange}
+                    editando={editando}
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+        <Snackbar 
+            open={openNotificacion} 
+            autoHideDuration={ 6000 } 
+            onClose={ handleCloseNotification }
+        >
+            <Alert 
+                onClose={handleCloseNotification} 
+                severity="error" 
+                sx={{ width: '100%' }}
+            >
                 { MensajeDeInformacion }
             </Alert>
         </Snackbar>
